e41120034bf00d1f0af0f917bb0c0fd045faa680
Edouard Rabany <edouard.rabany@piksel.com>
2014-07-18 15:10:58 +0200

    MongoDB
    Signed-off-by: Edouard Rabany <edouard.rabany@piksel.com>

--- web/web.js
+++ web/web.js
@@ -1,8 +1,22 @@
 var http = require("http");
 var fs = require('fs');
+var express = require("express");
 var counter = 0;
 var port = Number(process.env.PORT || 5000);
+var MongoClient = require('mongodb').MongoClient;// Retrieve
 
+
+var account = {
+	login : 'erabany',
+	password : 'zohon182'
+};
+
+// Connect to the db
+MongoClient.connect("mongodb://"+account.login+":"+account.password+"@ds061248.mongolab.com:61248/zohon", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});
 
 var webDirectory = "./web/";
 
